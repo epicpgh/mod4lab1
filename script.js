b1244886e78224d161c83c27a8f6a4c9a1719360
Evan Miller <ewmst5@gmail.com>
2025-05-14 22:46:38 -0400

    I am going to return to this to build out the event listeners and the html and css parts.

--- script.js
+++ script.js
@@ -4,32 +4,44 @@
     Create a function called formatFullName that takes two parameters: firstName and lastName. The function should return the full name in the format lastName, firstName.
     Format the firstName and lastName so that the first letter of each is always capatilized.
     Add a condition that checks if either the first or last name is missing. If so, return a string saying “Invalid name input.” */
-    // let firstName = "Ronin";
-    // let lastName = "Miller";
-    // let fullName = lastName + ', ' + firstName;
+    // const input = document.getElementById("nameInput");
+    // const nameBtn = document.getElementById('nameBtn');
+    // nameBtn.addEventListener('click', function(){
+    //         return input;
+    
+    //     });
+    
+    
+    
+    
+    
+    
+    let firstName = "Ronin";
+    let lastName = "Miller";
+    let fullName = lastName + ', ' + firstName;
 
 
-    // function formatFullName(lastName, firstName){
+    function formatFullName(lastName, firstName){
        
 
-    //    if(firstName == null || lastName ==null || firstName==""||lastName=="")
-    //    {
-    //    return 'Invalid Name Input';
-    // }
+       if(firstName == null || lastName ==null || firstName==""||lastName=="")
+       {
+       return 'Invalid Name Input';
+    }
 
-    //     else {
-    //         firstName.charAt(0).toUpperCase(); + firstName.slice(1);
-    //         lastName.charAt(0).toUpperCase();+ firstName.slice(1);
+        else {
+            firstName.charAt(0).toUpperCase(); + firstName.slice(1);
+            lastName.charAt(0).toUpperCase();+ firstName.slice(1);
         
-    //     let fullName = lastName + ', ' + firstName;
-    //     return fullName;}
-    //     }
+        let fullName = lastName + ', ' + firstName;
+        return fullName;}
+        }
     
 
         
- //console.log(formatFullName('Ronin','Miller'));
+ console.log(formatFullName('ronin','miller'));
     
-//    
+   
 // let p = Number('price');
 // let q = Number('quantity');
 // let t = Number('taxRate');
@@ -56,7 +68,7 @@
 
 
 
-// 
+
 
 
 //Task 3: Functions with Conditional Logic
@@ -68,21 +80,21 @@
 
 
 
-// function checkIfEligible(age , isEmployed){
-//     let a = Number(age);
-//     if (isNaN(a) || typeof isEmployed == false){
-//         return "Invalid Input";
-//     }
+function checkIfEligible(age , isEmployed){
+    let a = Number(age);
+    if (isNaN(a) || typeof isEmployed == false){
+        return "Invalid Input";
+    }
 
-//     if (a >= 18 && isEmployed){
-//         return "Is Eligible";
+    if (a >= 18 && isEmployed){
+        return "Is Eligible";
 
-//     } else {
-//         return "Not Eligible";
-//     }
-// }
+    } else {
+        return "Not Eligible";
+    }
+}
 
-// console.log(checkIfEligible(18, true));
+console.log(checkIfEligible(18, true));
 
 
 
